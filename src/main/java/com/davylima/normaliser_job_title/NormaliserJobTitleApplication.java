a1ca0df113bf3eb527d5c28caff79461d9e0e329
Davy Lima <davy.g.c.lima@gmail.com>
2024-07-17 08:32:25 -0300

    project set up

---
+++ src/main/java/com/davylima/normaliser_job_title/NormaliserJobTitleApplication.java
@@ -0,0 +1,16 @@
+package com.davylima.normaliser_job_title;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormaliserJobTitleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NormaliserJobTitleApplication.class, args);
+
+
+	}
+
+}
